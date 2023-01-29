4ada6d195f0cd0b0e042f6ca05386fb2166494d5
Aislan A <109553055+asN6a@users.noreply.github.com>
2023-01-29 11:35:41 -0300

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,7 @@
+let texts = ["DETERMINATION", "INSPIRATION", "AISLAN A."];
+let index = 0;
 
+setInterval(function() {
+  document.getElementById("text-time-m").innerHTML = texts[index];
+  index = (index + 1) % texts.length;
+}, 3000);
